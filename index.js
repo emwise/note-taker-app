f0a7277d249ece4466dc5ebc1d79a0dd5c8e187d
Emma Wise <30713299+emwise@users.noreply.github.com>
2022-07-16 19:16:45 -0500

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = 4000
 
+app.get('/', (req, res, next)=>{
+  res.send('hello')
+})
+
 app.listen(port, ()=>{
   console.log(`app is running on http://localhost:${port}`)
 })

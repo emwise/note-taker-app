5c2fe5f9796c6afd21ac1576ff0870af77c7ae28
Emma Wise <30713299+emwise@users.noreply.github.com>
2022-07-19 12:57:57 -0500

    port set to env variables

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 const noteRoutes = require('./notes/routes')
 const userRoutes = require('./users/routes')
 
-mongoose.connect('mongodb://localhost/notetaking') //this is the mongodb address.
+const { username, password } = process.env //accesses certain variables in my computer that this application can access
+//these are added via powershell like: $Env: username = 'username'
+//then run npm start as usual
+
+mongoose.connect(`mongodb+srv://${username}:${password}@maindatabase.zp5uc0f.mongodb.net/test`) //this is the mongodb address.
 //connect to this when you first open mongodb compass 
 
 app.use(bodyParser.json())
